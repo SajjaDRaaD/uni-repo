800f6f3f6404d9dac78e39d48b6a37ab3ab01ce4
Pisco <83592195+Piscoam@users.noreply.github.com>
2022-05-12 11:23:07 +0430

    Add files via upload

---
+++ magic code.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[2]:
+
+
+get_ipython().run_cell_magic('javascript', '', '')
+
+
+# In[3]:
+
+
+get_ipython().run_line_magic('store', '')
+
+
+# In[4]:
+
+
+get_ipython().run_line_magic('writefile', '')
+
+
+# In[5]:
+
+
+get_ipython().run_line_magic('timeit', '')
+
+
+# In[6]:
+
+
+get_ipython().run_line_magic('ismagic', '')
+
+
+# In[ ]:
+
+
+
+
