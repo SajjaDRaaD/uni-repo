3f469bb326d9dca70d09e29dcf41500c76cc18cc
maryam <103657696+yaghoobiii@users.noreply.github.com>
2022-05-12 11:37:29 +0430

    Add files via upload

---
+++ maryam yaghoobi.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is level 1 heading
+# # This is a level 2 heading
+# this is some plane text that forms a paragraf . add emphasis via **bold** and __ bold__, or *italic* and _italic_.
+# paragraph must be separated by an empty line.
+# * sometimes we want to include lists.
+# * which can be bulleted using asterisks.
+# 
+# 1. Lists can also be numbered.
+# 2. if we want an ordered list.
+# 
+# [It is possibal to include hyperlinks ](htttps://www.example.com)
+# Inline code uses single backticks: ' foo()', and code blocks use triple backticks:
+# 
+# bar()
+# 
+# Or can be indented by 4 spaces:
+# 
+#    
+#   foo()
+#     
+# And finally, adding images is easy: ![Alt taxt](https://www.example.com/image.jpg)
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
