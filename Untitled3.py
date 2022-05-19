d909f06416b99ca6cd1c98f1d64f5ea398a48dc6
alirezanaserii <103682776+alirezanaserii@users.noreply.github.com>
2022-05-19 09:55:20 +0430

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+L = ["malard university" ,607, "class2005" ,789.009]
+L[2]
+
+
+# In[4]:
+
+
+L = ["malard univercity" ,607 ,["class2005" ,18] ,789.009]
+L[4][3]
+
+
+# In[5]:
+
+
+L = ["malard univercity" ,607 ,["class2005" ,18] ,789.009]
+L[1] = karaj ;
+
+
+# In[6]:
+
+
+L = ["malard univercity" ,607 ,["class2005" ,18] ,789.009]
+L[1] = karaj ;
+L[1]
+"karaj"
+
+
+# In[7]:
+
+
+L = ["malard univercity" ,607 ,["class2005" ,18] ,789.009]
+L[1] = 'karaj'
+L[1]
+'karaj'
+
+
+# In[11]:
+
+
+L = [1 ,2 ,3 ,4]
+L[1:0] 
+
+
+# In[16]:
+
+
+L = [1 ,2 ,3 ,4 ,5]
+L[0:3]
+
+
+# In[ ]:
+
+
+
+
