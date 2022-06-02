ebbc24cad48eec1f3a0d54f07eb56b54769f9531
Mahshid Nabavi <103659702+MahshidNabavi@users.noreply.github.com>
2022-06-02 21:38:08 +0430

    1401/03/12 train

---
+++ دستورات مرکب.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+if x > y :
+    if z > x :
+        print("z is greatest.")
+    print("x is greater than y.")
+print("found the greatest.")
+
+
+# In[10]:
+
+
+if x > y :
+    print("x is greater than y.")
+else:
+    print("y is greater than x.")
+
+
+# In[18]:
+
+
+if x > y :
+    x = 0
+    y += 1
+    z = x + y
+    age = 5
+
+
+# In[ ]:
+
+
+
+
