5f721ee6e79f9c95d91d04b6e1811fb26b3320aa
Armin <103656430+Arminmh79@users.noreply.github.com>
2022-06-02 09:33:14 +0430

    Add files via upload

---
+++ دستورات مرکب if.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+if x > y :
+    x = 0
+    y += 1
+    z = x + y
+age = 5
+    def move()
+
+
+# In[15]:
+
+
+if x > y :
+    if z > x :
+      print("z is greatest.")
+    print("x is greater than y.")
+print("found the greatest.")
+
+
+# In[14]:
+
+
+if x > y :
+    print("x is greater than y.")
+else:
+    print("y is greater than x.")
+    
+
+
+# In[ ]:
+
+
+
+
