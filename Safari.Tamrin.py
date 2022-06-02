b23693a9311fead7e9c000ba3fa1111e46692cf2
Kimiasafari <103957546+Kimiasafari@users.noreply.github.com>
2022-06-03 03:18:12 +0430

    Add files via upload

---
+++ Safari.Tamrin.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+L= ["malarduniversity ", 607 , " clss2005" , 789.009]
+L[0]
+
+
+# In[2]:
+
+
+L= ["malarduniversity ", 607 ,['CLASS',18], " clss2005" , 789.009]
+L[2][1]
+
+
+# In[3]:
+
+
+L= ["malarduniversity ", 607 , " clss2005" , 789.009]
+L[1:4]
+
+
+# In[5]:
+
+
+L= ["malarduniversity ", 607 , " clss2005" , 789.009]
+L[1] = "TEHRAN"
+L[1] 
+
+
+# In[6]:
+
+
+
+L=[1,2,3,4,5]
+L[0:3]
+
+
+# In[ ]:
+
+
+
+
