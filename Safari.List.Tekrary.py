cff2cfad6f86a1d67d1c3cf44580026f75d1805d
Kimiasafari <103957546+Kimiasafari@users.noreply.github.com>
2022-06-03 02:28:29 +0430

    List.Tekrary

---
+++ Safari.List.Tekrary.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def all_unique(lst):
+    return len (lst) == len(set(lst))
+
+x=[1,1,2,2,3,2,4,5,6]
+y=[1,2,3,4,5]
+all_unique(x) #false
+all_unique(y) #true
+
+
+# In[ ]:
+
+
+
+
