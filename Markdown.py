53ac3903607443a0de3936436a85cf59bdfceb35
Mahshid Nabavi <103659702+MahshidNabavi@users.noreply.github.com>
2022-06-03 03:09:20 +0430

    Mahshid Nabavi (Markdown)

--- Markdown.py
+++ Markdown.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # This is a level 1 heading ## This is a level 2 heading 
-# This is some plain text that forms a paragraph. Add emphasis via **bold** and __bold__, or *italic* and _italic_.  Paragraphs must be separated by an empty line.  
-# * Sometimes we want to include lists.  * Which can be bulleted using asterisks.  
-# 1. Lists can also be numbered.
-# 2. If we want an ordered list. [It is possible to include hyperlinks](https://www.example.com) 
-# Inline code uses single backticks: `foo()`, and code blocks use triple backticks:  ``` 
-# bar() ```  Or can be indented by 4 spaces:      foo()
-# 
-# 
+# In[1]:
 
-# 
+
+
+
+#this is a level 1 healing
+##this is a level 2 healing
+this is some plain text that forms a paragraph.Add emphasis via**bold**and _bold_,or*italic*and_italic_.
+paragraphs must be separated by an empty line.
+*Sometimes we want to include lists.
+*Which can be bulleted using asterisks.
+1.Lists can also be numbered.
+2.If we want an ordered list.
+
+[It is possible to include hyperlinks](https://www.example.com)
+Inline code uses single backticks:'foo()',and code blocks use triple backticks:
+ '''       
+ bar()
+ '''
+
+or can be indented by 4 spaces:
+    
+    foo()
+
+    And finally,addhng images is easy: ![Alt text](https://www.example.com/image.jpg) 
+        
+
+
+# In[ ]:
+
+
+
+
